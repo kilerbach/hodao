ab18858caa0d82805496ea4826b1a5b5be91e34e
chenwenda <chenwenda@kingsoft.com>
2014-09-13 12:31:16 +0800

    status while error

--- hodao/views/error.py
+++ hodao/views/error.py
@@ -18,17 +18,17 @@ _logger = logging.getLogger(__name__)
 @application.errorhandler(500)
 def error_handler(ex):
     _logger.error("Exception <%s>, Traceback: %s", str(ex), traceback.format_exc())
-    return render_template('error.html', msg=u'服务器异常')
+    return render_template('error.html', msg=u'服务器异常'), 500
 
 
 @application.errorhandler(400)
 def handle_400(ex):
-    return render_template('error.html', msg=u'参数错误')
+    return render_template('error.html', msg=u'参数错误'), 400
 
 
 @application.errorhandler(404)
 def handle_404(ex):
-    return render_template('error.html', msg=u'没有找到该网页')
+    return render_template('error.html', msg=u'没有找到该网页'), 404
 
 
 @application.before_request
